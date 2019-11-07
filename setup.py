26ee5dd4b78c2bd48388752f988fb950c095a95b
Kent Ross <k@mad.cash>
2019-11-07 11:13:53 -0800

    add classifiers for newer python versions

--- setup.py
+++ setup.py
@@ -15,6 +15,8 @@ setup(
         'Programming Language :: Python :: 3 :: Only',
         'Programming Language :: Python :: 3.6',
         'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
         'License :: OSI Approved :: MIT License',
     ],
 
